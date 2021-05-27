9188dd81cc59dcc14a1211855c86c7b56b0c9092
Brandon Copeland <copelandbrandon2@gmail.com>
2021-05-27 17:53:37 +0000

    will refactor reservations link to use sql query instead of hardcoded data

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -68,7 +68,13 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT * FROM reservations where guest_id = $1 LIMIT $2`, [guest_id, limit])
+    .then(reservations => {
+      console.log(reservations.rows);
+      return reservations.rows;
+    })
+    .catch((err)=> console.log('query error: ', err.message));
 }
 exports.getAllReservations = getAllReservations;
 
